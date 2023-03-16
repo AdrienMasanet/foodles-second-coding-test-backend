2c28d55de32b453b26541c816334adbc0b8251d9
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-17 00:14:48 +0100

    feat: client model

---
+++ foodles_second_coding_test_backend/client/models.py
@@ -0,0 +1,13 @@
+import uuid
+from django.db import models
+
+
+class Client(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField()
+    credits = models.IntegerField(default=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
