2b308135a1c27f3ae2a093a63a7bd5700ec0022a
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-17 12:02:43 +0100

    fix: make email unique

--- foodles_second_coding_test_backend/client/models.py
+++ foodles_second_coding_test_backend/client/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Client(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     credits = models.IntegerField(default=30)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
