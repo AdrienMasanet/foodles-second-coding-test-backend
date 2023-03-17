4cbd0a4450ab151bbc7d96be789bd60a24dbce14
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-17 02:19:32 +0100

    test: add view related tests

--- foodles_second_coding_test_backend/client/tests.py
+++ foodles_second_coding_test_backend/client/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 
 from client.models import Client
+from client.views import ClientsListView, ClientView, ClientsSeederView, ClientDeleteAllView, ClientDeleteView
 
 
-class ClientTestCase(TestCase):
+# Tests for the client model and ORM
+class ClientTestOrmCase(TestCase):
     # The clients should be created with the correct attributes
     def test_create_client(self):
         database_clients_count = Client.objects.count()
@@ -45,3 +47,46 @@ class ClientTestCase(TestCase):
         client.delete()
 
         self.assertEqual(Client.objects.count(), database_clients_count - 1)
+
+
+# Tests for the client views
+class ClientTestViewCase(TestCase):
+    # The clients should be listed
+    def test_list_clients(self):
+        client = Client.objects.create(
+            email="client@foodles.fr",
+            credits=30,
+        ).save()
+
+        response = self.client.get("/clients/")
+        self.assertEqual(response.status_code, 200)
+
+    # A client must be viewable
+    def test_view_client(self):
+        client = Client.objects.create(
+            email="client@foodles.fr",
+            credits=30,
+        ).save()
+
+        response = self.client.get("/clients/" + str(Client.objects.last().id))
+        self.assertEqual(response.status_code, 200)
+
+    # All the clients should be deleted
+    def test_delete_all_clients(self):
+        client = Client.objects.create(
+            email="client1@foodles.fr",
+            credits=30,
+        ).save()
+
+        client = Client.objects.create(
+            email="client2@foodles.fr",
+            credits=30,
+        ).save()
+
+        client = Client.objects.create(
+            email="client3@foodles.fr",
+            credits=30,
+        ).save()
+
+        response = self.client.delete("/clients/delete-all")
+        self.assertEqual(response.status_code, 200)
