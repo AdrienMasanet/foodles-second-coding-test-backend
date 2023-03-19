e99809df793d494dbdea7b3ef0bc9662a9041538
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-19 17:37:17 +0100

    feat: price is 0 by default

--- foodles_second_coding_test_backend/product/models.py
+++ foodles_second_coding_test_backend/product/models.py
@@ -9,7 +9,7 @@ from django.conf import settings
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     description = models.TextField()
     image = models.ImageField(upload_to="product_images/")
     stock = models.IntegerField(default=0)
