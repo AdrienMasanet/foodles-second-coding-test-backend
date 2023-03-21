4c3af207209e97d305eb0a6edee23f3d11ad7d66
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-21 23:41:16 +0100

    feat: order products by name on the ProductsListView

--- foodles_second_coding_test_backend/product/views.py
+++ foodles_second_coding_test_backend/product/views.py
@@ -10,6 +10,6 @@ from .serializers import ProductSerializer
 class ProductsListView(APIView):
     def get(self, request):
         # Get all products in stock (stock > 0)
-        products = Product.objects.filter(stock__gt=0)
+        products = Product.objects.filter(stock__gt=0).order_by("name")
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data or {"error": "No products found"})
