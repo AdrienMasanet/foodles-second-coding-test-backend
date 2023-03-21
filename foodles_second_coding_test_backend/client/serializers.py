1b16e066bd9179ff4780360a16aa114f76d696d3
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-21 12:29:21 +0100

    feat: add ClientExplicitSerializer to provide more personal infos

--- foodles_second_coding_test_backend/client/serializers.py
+++ foodles_second_coding_test_backend/client/serializers.py
@@ -6,4 +6,13 @@ from .models import Client
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ["id", "email", "credits"]
+        fields = ["id", "name", "email", "credits"]
+
+
+class ClientExplicitSerializer(serializers.ModelSerializer):
+    createdAt = serializers.DateTimeField(source="created_at")
+    updatedAt = serializers.DateTimeField(source="updated_at")
+
+    class Meta:
+        model = Client
+        fields = ["id", "name", "email", "credits", "createdAt", "updatedAt"]
