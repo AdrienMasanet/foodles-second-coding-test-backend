39fda3df3574a5dd35d9ecbe6b01b471669296ab
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-21 19:13:31 +0100

    fix: use a DecimalField instead of an IntegerField for client credits

--- foodles_second_coding_test_backend/client/models.py
+++ foodles_second_coding_test_backend/client/models.py
@@ -6,7 +6,7 @@ class Client(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    credits = models.IntegerField(default=30)
+    credits = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     session_token = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
